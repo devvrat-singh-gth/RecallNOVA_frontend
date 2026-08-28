54174e2ce8733ef838f5889eba6706c88b1af9f0
devvrat-singh-gth <devvratsinghbanga2005@gmail.com>
2026-08-29 01:20:40 +0530

    v2.2

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
-import { useAuth } from "./AuthProvider";
+import { useAuth } from "./auth/AuthProvider";
 
 export default function ProtectedRoute({
   children,
